9570efb7dd21bf8186375a06781b24fdd234819b
runklebc <runklebc@miamioh.edu>
2014-07-31 11:19:23 -0400

    Update SearchTweets.java

--- SearchTweets.java
+++ SearchTweets.java
@@ -15,7 +15,7 @@ import java.util.Set;
  
 public class SearchTweets {
 
-        private final static String CONSUMER_KEY = "myKey";
+        private final static String CONSUMER_KEY        = "myKey";
         private final static String CONSUMER_KEY_SECRET = "mySecret";
         public static Twitter twitter = new TwitterFactory().getInstance();
  
